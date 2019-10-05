304dea285466ee9f32bb29e04dd69f343cdd4e18
Jeremy Knight <jeremy@theknights.me>
2019-10-05 16:02:49 -0500

    added tests in both xunit and mstest

---
+++ unit-testing-2019/UnitTesting/SystemUnderTest.Tests.MSTest/ArithmeticTests.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace SystemUnderTest.Tests.MSTest
+{
+    [TestClass]
+    public class ArithmeticTests
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(AddData), DynamicDataSourceType.Property)]
+        public void Add_GivenData(decimal left, decimal right, decimal expected)
+        {
+            // arrange - done by data rows
+
+            // act 
+            decimal actual = Arithmetic.Add(left, right);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        public static IEnumerable<object[]> AddData
+        {
+            get
+            {
+                yield return new object[] { 1m, 2m, 3m };
+                yield return new object[] { 2m, 2m, 4m };
+            }
+        }
+
+        [TestMethod]
+        public void Divide_ValidNumber_CalculatedValue()
+        {
+            // arrange
+            decimal left = 6;
+            decimal right = 2;
+
+            // act 
+            decimal actual = Arithmetic.Divide(left, right);
+
+            // assert
+            Assert.AreEqual(3, actual);
+        }
+
+        [TestMethod]
+        public void Divide_ZeroRightSide_DivideByZeroException()
+        {
+            // arrange
+            decimal left = 1;
+            decimal right = 0;
+
+            // act 
+            Action action = () => Arithmetic.Divide(left, right);
+
+            // assert
+            var exception = Assert.ThrowsException<DivideByZeroException>(action);
+            Assert.AreEqual("Attempted to divide by zero.", exception.Message);
+        }
+
+        [TestMethod]
+        public void Multiply_ValidNumber_CalculatedValue()
+        {
+            // arrange
+            decimal left = 7;
+            decimal right = 4;
+
+            // act 
+            decimal actual = Arithmetic.Multiply(left, right);
+
+            // assert
+            Assert.AreEqual(28, actual);
+        }
+
+        [TestMethod]
+        public void Subtract_ValidNumber_CalculatedValue()
+        {
+            // arrange
+            decimal left = 8;
+            decimal right = 3;
+
+            // act 
+            decimal actual = Arithmetic.Subtract(left, right);
+
+            // assert
+            Assert.AreEqual(5, actual);
+        }
+    }
+}
---
+++ unit-testing-2019/UnitTesting/SystemUnderTest.Tests.xUnit/ArithmeticTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace SystemUnderTest.Tests.xUnit
+{
+    public class ArithmeticTests
+    {
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(2, 2, 4)]
+        public void Add_GivenTheories(decimal left, decimal right, decimal expected)
+        {
+            // arrange - done by theories
+            // act 
+            decimal actual = Arithmetic.Add(left, right);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(6, 2, 3)]
+        public void Divide_GivenTheories(decimal left, decimal right, decimal expected)
+        {
+            // arrange - done by theories
+            // act 
+            decimal actual = Arithmetic.Divide(left, right);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Divide_ZeroRightSide_DivideByZeroException()
+        {
+            // arrange
+            decimal left = 1;
+            decimal right = 0;
+
+            // act 
+            Action action = () =>
+            {
+                _ = Arithmetic.Divide(left, right);
+            };
+
+            // assert
+            var exception = Assert.Throws<DivideByZeroException>(action);
+            Assert.Equal("Attempted to divide by zero.", exception.Message);
+        }
+
+        [Theory]
+        [InlineData(7, 4, 28)]
+        public void Multiply_GivenTheories(decimal left, decimal right, decimal expected)
+        {
+            // arrange - done by theories
+            // act 
+            decimal actual = Arithmetic.Multiply(left, right);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(SubtractData))]
+        public void Subtract_GivenTheories(decimal left, decimal right, decimal expected)
+        {
+            // arrange - done by theories
+            // act 
+            decimal actual = Arithmetic.Subtract(left, right);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        public static IEnumerable<object[]> SubtractData
+        { 
+            get
+            {
+                yield return new object[] { 8, 3, 5 };
+                yield return new object[] { 4, 4, 0 };
+            }
+        }
+
+    }
+}
