55456d1fa1b7a29bb20e46639b6c41c9b9771d58
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2022-05-09 17:21:22 -0500

    updated unit test presentation code for 2022 version

---
+++ 2022-unit-testing/SystemUnderTest/Data/PersonUpdateCommand.cs
@@ -0,0 +1,30 @@
+﻿namespace SystemUnderTest.Data;
+
+public class PersonUpdateCommand
+{
+    private readonly IGetPersonByIdQuery personRepo;
+
+    public PersonUpdateCommand() : this(new GetPersonByIdQuery())
+    {
+    }
+
+    public PersonUpdateCommand(IGetPersonByIdQuery personRepository)
+    {
+        this.personRepo = personRepository;
+    }
+
+    public bool Execute(Person person)
+    {
+        // run validation logic
+
+        var dbPerson = this.personRepo.GetById(person.Id);
+        if (dbPerson == null)
+        {
+            return false;
+        }
+
+        // run update logic 
+
+        return true;
+    }
+}
