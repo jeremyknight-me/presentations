7b06bdb928eeece03c2e7a0b7d29b9446719edaf
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2022-05-09 17:39:40 -0500

    added date time provider example

---
+++ 2022-unit-testing/SystemUnderTest/AgeCalculator.cs
@@ -0,0 +1,53 @@
+﻿using JK.Common.DateTimeProviders;
+
+namespace SystemUnderTest;
+
+#region No Provider
+
+public class AgeCalculator
+{
+    public int Calculate(DateTime birthday)
+    {
+        var now = DateTime.Today;
+        var age = now.Year - birthday.Year;
+        if (birthday > now.AddYears(-age))
+        {
+            age--;
+        }
+
+        return age;
+    }
+}
+
+#endregion
+
+#region Provider
+
+//public class AgeCalculator
+//{
+//    private readonly IDateTimeProvider dateTime;
+
+//    public AgeCalculator(IDateTimeProvider dateTimeProvider)
+//    {
+//        this.dateTime = dateTimeProvider;
+//    }
+
+//    public AgeCalculator() : this(new DefaultDateTimeProvider())
+//    {
+//    }
+
+//    public int Calculate(in DateTime birthday)
+//    {
+//        var now = this.dateTime.Today;
+//        var age = now.Year - birthday.Year;
+//        if (birthday > now.AddYears(-age))
+//        {
+//            age--;
+//        }
+
+//        return age;
+//    }
+//}
+
+#endregion
+
