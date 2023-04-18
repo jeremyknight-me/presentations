a905513d4ca28c58cd77e889dc17acd1ca8b6fe0
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2023-04-17 22:12:36 -0500

    built testing your test projects

---
+++ 2023-in-depth-testing/TestingYourTests.Tests/CalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿namespace TestingYourTests.Tests;
+
+public class CalculatorTests
+{
+	private readonly Calculator sut;
+
+    public CalculatorTests()
+    {
+		this.sut = new();
+    }
+
+    [Theory]
+	[InlineData(5, 5, 10)]
+	public void Add_Theories(int first, int second, int expected)
+	{
+		var actual = sut.Add(first, second);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData(5, 5, 0)]
+	public void Subtract_Theories(int first, int second, int expected)
+	{
+		var actual = sut.Subtract(first, second);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData(1, 1, 1)]
+	public void Multiply_Theories(int first, int second, int expected)
+	{
+		var actual = this.sut.Multiply(first, second);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData(1, 1, 1, 0)]
+	public void Divide_Theories(int first, int second, int expected, int expectedRemainder)
+	{
+		var actual = this.sut.Divide(first, second);
+		Assert.Equal(expected, actual.Result);
+		Assert.Equal(expectedRemainder, actual.Remainder);
+	}
+}
---
+++ 2023-in-depth-testing/TestingYourTests/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace TestingYourTests;
+
+public sealed class Calculator
+{
+	public int Add(int first, int second)
+		=> first + second;
+
+	public int Subtract(int first, int second)
+		=> first - second;
+
+	public int Multiply(int first, int second)
+		=> first * second;
+
+	public (int Result, int Remainder) Divide(int first, int second)
+	{
+		if (second == 0)
+		{
+			throw new DivideByZeroException();
+		}
+
+		var result = first / second;
+		var remainder = first % second;
+		return (result, remainder);
+	}
+}
