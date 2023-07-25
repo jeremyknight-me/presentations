4c344e0bc10a51e2705d34ae024a5a8dab547689
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2023-07-25 13:34:26 -0500

    updated query example

--- 2023-basics-adonet/BackToBasicsAdoNet/Examples/QueryExample.cs
+++ 2023-basics-adonet/BackToBasicsAdoNet/Examples/QueryExample.cs
@@ -8,24 +8,29 @@ internal static class QueryExample
 {
 	internal static void Run(ConnectionStrings connectionStrings)
     {
-		using var connection = new SqlConnection(connectionStrings.Simple);
-		using SqlCommand command = connection.CreateCommand();
-		command.CommandType = CommandType.Text; // or StoredProcedure
-		command.CommandText = "SELECT Id, Name FROM [dbo].[Lookup] WHERE IsDeleted = 0";
-
-		if (connection.State != ConnectionState.Open)
+		using (var connection = new SqlConnection(connectionStrings.Simple)) 
+		using (SqlCommand command = connection.CreateCommand())
 		{
-			connection.Open();
-		}
+			command.CommandType = CommandType.Text; // or StoredProcedure
+			command.CommandText = "SELECT Id, Name FROM [dbo].[Lookups] WHERE IsDeleted = 0";
 
-		using var dataReader 
-			= command.ExecuteReader(CommandBehavior.SingleResult | CommandBehavior.CloseConnection);
-		while (dataReader.Read())
-		{
-			var id = dataReader["Id"];
-			var name = dataReader["Name"];
-			// Console.WriteLine handles a lot of additional parsing, checks, etc. that would normally be done.
-			Console.WriteLine($"Id = {id} | Name = {name}");
+			if (connection.State != ConnectionState.Open)
+			{
+				connection.Open();
+			}
+
+			using (var dataReader
+				= command.ExecuteReader(CommandBehavior.SingleResult | CommandBehavior.CloseConnection))
+			{
+				while (dataReader.Read())
+				{
+					var id = dataReader["Id"];
+					var name = dataReader["Name"];
+					// Console.WriteLine handles a lot of additional parsing, checks, etc. that would normally be done.
+					Console.WriteLine($"Id = {id} | Name = {name}");
+				}
+			}
 		}
+		
 	}
 }
