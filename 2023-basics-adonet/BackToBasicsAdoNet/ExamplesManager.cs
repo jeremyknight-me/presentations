999502044f42ba45c81bf1d384dea73dc2a96827
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2024-02-20 17:48:15 -0600

    updated examples manager

--- 2023-basics-adonet/BackToBasicsAdoNet/ExamplesManager.cs
+++ 2023-basics-adonet/BackToBasicsAdoNet/ExamplesManager.cs
@@ -9,21 +9,21 @@ internal static class ExamplesManager
 	internal static void Run()
 	{
         var connectionStrings = ConnectionStrings.Create();
-		var enabled = true;
+		var enabled = 1;
 
-		if (!enabled) { QueryExample.Run(connectionStrings); }
-		if (!enabled) { QueryByIdExample.Run(connectionStrings); }
+		if (enabled == 1) { QueryExample.Run(connectionStrings); }
+		if (enabled == 2) { QueryByIdExample.Run(connectionStrings); }
 
-		if (!enabled) { InsertExample.Run(connectionStrings); }
-		if (!enabled) { BulkCopyExample.Run(connectionStrings); }
+		if (enabled == 3) { InsertExample.Run(connectionStrings); }
+		if (enabled == 4) { BulkCopyExample.Run(connectionStrings); }
 
-		if (!enabled) { UpdateExample.Run(connectionStrings); }
-		if (!enabled) { DeleteExample.Run(connectionStrings); }
+		if (enabled == 5) { UpdateExample.Run(connectionStrings); }
+		if (enabled == 6) { DeleteExample.Run(connectionStrings); }
 
-		if (!enabled) { IntegrationExample.Run(connectionStrings); }
+		if (enabled == 7) { IntegrationExample.Run(connectionStrings); }
 
-		if (!enabled) { BulkInsertExample.Run(connectionStrings); }
-		if (!enabled) { QueryMultipleExample.Run(connectionStrings); }
+		if (enabled == 8) { BulkInsertExample.Run(connectionStrings); }
+		if (enabled == 9) { QueryMultipleExample.Run(connectionStrings); }
 	}
 }
 
